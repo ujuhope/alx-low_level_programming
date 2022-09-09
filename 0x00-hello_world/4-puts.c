c010c6d30842de885e4b20512a5b56f78bfef94b
ujuhope <chukwuhope95@gmail.com>
2022-09-08 18:02:21 -0700

    putv

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stdio.h>                                           
-/**                                                          
- * main - using the put function to print string             
- *                                                           
- * Description: using the main function to print             
- * Programming is like building a multilingual puzzle       
- * Return: 0                                                
- */                                                          
-int main(void)                                               
-{                                                            
-        puts("\"Programming is like building a multilingual p
-uzzle");                                                     
-                return (0);                                  
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
